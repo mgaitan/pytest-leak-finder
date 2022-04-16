0b2c7a59948c37d765966ad15e1f7776576ee7bc
Martín Gaitán <gaitan@gmail.com>
2022-04-16 17:28:52 -0300

    WORK IN PROGRRESS based on stepwise plugin

--- pytest_leak_finder.py
+++ pytest_leak_finder.py
@@ -1,21 +1,115 @@
 # -*- coding: utf-8 -*-
 
+from typing import TYPE_CHECKING, List, Optional
+
 import pytest
+from _pytest import nodes
+from _pytest.config import Config
+from _pytest.config.argparsing import Parser
+from _pytest.main import Session
+from _pytest.reports import TestReport
+
+if TYPE_CHECKING:
+    from _pytest.cacheprovider import Cache
+
+CACHE_DIR = "cache/leak-finder"
 
 
-def pytest_addoption(parser):
-    group = parser.getgroup('leak_finder')
+def pytest_addoption(parser: Parser) -> None:
+    group = parser.getgroup("general")
     group.addoption(
-        '--foo',
-        action='store',
-        dest='dest_foo',
-        default='2022',
-        help='Set the value for the fixture "bar".'
+        "--leak-finder",
+        action="store_true",
+        default=False,
+        dest="leakfinder",
+        help="Bisect previous passed tests until find one that fail",
     )
 
-    parser.addini('HELLO', 'Dummy pytest.ini setting')
+
+@pytest.hookimpl
+def pytest_configure(config: Config) -> None:
+    if config.getoption("leakfinder"):
+        config.pluginmanager.register(LeakFinderPlugin(config), "leakfinderplugin")
 
 
-@pytest.fixture
-def bar(request):
-    return request.config.option.dest_foo
+def pytest_sessionfinish(session: Session) -> None:
+    if not session.config.getoption("leakfinder"):
+        assert session.config.cache is not None
+        # Clear the list of failing tests if the plugin is not active.
+        session.config.cache.set(CACHE_DIR, [])
+
+
+class LeakFinderPlugin:
+    def __init__(self, config: Config) -> None:
+        self.config = config
+        self.session: Optional[Session] = None
+        self.report_status = ""
+        assert config.cache is not None
+        self.cache: Cache = config.cache
+        self.lastfailed: Optional[str] = self.cache.get(CACHE_DIR, None)
+
+    def pytest_sessionstart(self, session: Session) -> None:
+        self.session = session
+
+    def pytest_collection_modifyitems(
+        self, config: Config, items: List[nodes.Item]
+    ) -> None:
+        if not self.lastfailed:
+            self.report_status = "no previously failed tests, not skipping."
+            return
+
+        target = self.cache.get("cache/leakfinder-target", {})
+
+        import ipdb
+
+        ipdb.set_trace()
+        # check all item nodes until we find a match on last failed
+        failed_index = None
+        for index, item in enumerate(items):
+            if item.nodeid == self.lastfailed:
+                failed_index = index
+                break
+
+        # If the previously failed test was not found among the test items,
+        # do not skip any tests.
+        if failed_index is None :
+            self.report_status = "previously failed test not found, not skipping."
+        else:
+            # deselect group B
+            total_target = items[:failed_index]
+            group_A = items[:len(total_target)//2] 
+            group_B = items[len(total_target)//2:-1]
+        
+            # deselect group B
+            del items[len(total_target)//2:-1]
+            import ipdb;ipdb.set_trace()
+            
+            # deselect group A
+            # del items[:len(total_target)//2]
+            config.hook.pytest_deselected(items=group_B)
+
+            # TODO set in cache wich group we actually executed
+            # if everything passes, we'll take the other group next run (plus the failed target)
+            # if it fails, we split the group again
+
+    def pytest_runtest_logreport(self, report: TestReport) -> None:
+        if report.failed:
+            # Mark test as the last failing and interrupt the test session.
+            self.lastfailed = report.nodeid
+            assert self.session is not None
+            self.session.shouldstop = (
+                "Test failed, continuing from this test next run."
+            )
+
+        elif report.when == "call":      # If the test was actually run and did pass.
+            # Remove test from the failed ones, if exists.
+            if report.nodeid == self.lastfailed:
+                self.lastfailed = None
+
+    def pytest_report_collectionfinish(self) -> Optional[str]:
+        if self.config.getoption("verbose") >= 0 and self.report_status:
+            return f"leakfinder: {self.report_status}"
+        return None
+
+    def pytest_sessionfinish(self) -> None:
+        self.cache.set(CACHE_DIR, self.lastfailed)
--- setup.py
+++ setup.py
@@ -1,50 +1,52 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os
 import codecs
+import os
+
 from setuptools import setup
 
 
 def read(fname):
     file_path = os.path.join(os.path.dirname(__file__), fname)
-    return codecs.open(file_path, encoding='utf-8').read()
+    return codecs.open(file_path, encoding="utf-8").read()
 
 
 setup(
-    name='pytest-leak_finder',
-    version='0.1.0',
-    author='Martín Gaitán',
-    author_email='gaitan@gmail.com',
-    maintainer='Martín Gaitán',
-    maintainer_email='gaitan@gmail.com',
-    license='MIT',
-    url='https://github.com/mgaitan/pytest-leak_finder',
-    description='Find the needle in the haystack',
-    long_description=read('README.rst'),
-    py_modules=['pytest_leak_finder'],
-    python_requires='>=3.5',
-    install_requires=['pytest>=3.5.0'],
+    name="pytest-leak-finder",
+    version="0.1.0",
+    author="Martín Gaitán",
+    author_email="gaitan@gmail.com",
+    maintainer="Martín Gaitán",
+    maintainer_email="gaitan@gmail.com",
+    license="MIT",
+    url="https://github.com/mgaitan/pytest-leak_finder",
+    description="Find the needle in the haystack",
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
+    py_modules=["pytest_leak_finder"],
+    python_requires=">=3.5",
+    install_requires=["pytest>=3.5.0"],
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Framework :: Pytest',
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Testing',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-        'Operating System :: OS Independent',
-        'License :: OSI Approved :: MIT License',
+        "Development Status :: 4 - Beta",
+        "Framework :: Pytest",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Testing",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
     ],
     entry_points={
-        'pytest11': [
-            'leak_finder = pytest_leak_finder',
+        "pytest11": [
+            "leak_finder = pytest_leak_finder",
         ],
     },
 )
